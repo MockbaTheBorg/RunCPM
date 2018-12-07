2d461c2b9ac222e9a37694983f501e6b799aa0f0
Mockba the Borg <MockbaTheBorg@users.noreply.github.com>
2018-12-07 08:49:34 -0500

    Improved handling of internal CCP

--- RunCPM/globals.h
+++ RunCPM/globals.h
@@ -36,7 +36,7 @@
 //
 #ifdef CCP_INTERNAL
 #define CCPname		"INTERNAL v1.5"			// Will use the CCP from ccp.h
-#define VersionCCP	0x15
+#define VersionCCP	0x15              // 0x10 and above reserved for Internal CCP
 #define BatchFCB	(tmpFCB + 36)
 #define CCPaddr		(BDOSjmppage-0x0800)
 #endif
