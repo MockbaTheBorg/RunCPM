5b71a7c6622ab6fde24a23ce4f31d95dd5f7c1c6
Philip Zembrod <pzembrod@gmail.com>
2023-12-17 12:40:50 +0100

    Add error handling for EOF on stdin to console.h

--- RunCPM/console.h
+++ RunCPM/console.h
@@ -81,6 +81,18 @@ uint8 _chready(void)		// Checks if there's a character ready for input
 {
 #ifdef STREAMIO
 	if (streamInActive) return 0xff;
+	// TODO: delegate to abstrction_posix.h function canReadFromKbd() or
+	// something similar.
+	// On Posix, if !streamInActive && streamInFile == stdin, this means
+	// EOF on stdin. Assuming that stdin is connected to a file or pipe,
+	// further reading from stdin won't read from the keyboard but just
+	// continue to yield EOF.
+	// On Windows, this problem doesn't exist because of the separete
+	// conio.h.
+	if (streamInFile == stdin) {
+		_puts("EOF on console input from stdin\n");
+		exit(EXIT_FAILURE);
+	}
 #endif
 	return(_kbhit() ? 0xff : 0x00);
 }
@@ -89,6 +101,7 @@ uint8 _getconNB(void)	  // Gets a character, non-blocking, no echo
 {
 #ifdef STREAMIO
 	if (streamInActive) return _getStreamInChar();
+	// TODO: Consider adding canReadFromKbd() here, too.
 #endif
 	return(_kbhit() ? _getch() : 0x00);
 }
@@ -97,6 +110,7 @@ uint8 _getcon(void)	   // Gets a character, blocking, no echo
 {
 #ifdef STREAMIO
 	if (streamInActive) return _getStreamInChar();
+	// TODO: Consider adding canReadFromKbd() here, too.
 #endif
 	return _getch();
 }
@@ -105,6 +119,7 @@ uint8 _getconE(void)   // Gets a character, blocking, with echo
 {
 #ifdef STREAMIO
 	if (streamInActive) return _getStreamInCharEcho();
+	// TODO: Consider adding canReadFromKbd() here, too.
 #endif
 	return _getche();
 }
